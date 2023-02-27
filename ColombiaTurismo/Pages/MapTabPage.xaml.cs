f5ce248abe3c805b73e40b323acce50b00adb840
brian593 <brian593@live.com>
2023-02-26 21:29:37 -0500

    description page ok

--- ColombiaTurismo/Pages/MapTabPage.xaml.cs
+++ ColombiaTurismo/Pages/MapTabPage.xaml.cs
@@ -1,12 +1,43 @@
-﻿namespace ColombiaTurismo.Pages;
+﻿using Mapsui;
+using Mapsui.Layers;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using Mapsui.UI.Maui;
+using static Google.Android.Material.Tabs.TabLayout;
+
+namespace ColombiaTurismo.Pages;
 
 public partial class MapTabPage : ContentPage
 {
 	public MapTabPage()
 	{
 		InitializeComponent();
-        var mapControl = new Mapsui.UI.Maui.MapControl();
-        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
-        Content = mapControl;
+
+
+
+        //var mapView = new Mapsui.UI.Maui.MapControl();
+
+
+        //    mapView.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+
+
+        mapView.Map.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+
+        mapView.MyLocationLayer.UpdateMyLocation(new Mapsui.UI.Maui.Position());
+
+        mapView.IsZoomButtonVisible = true;
+        mapView.IsMyLocationButtonVisible = true;
+        mapView.IsNorthingButtonVisible = true;
+
+                //    Content = mapControl;
+
+
+        var Lista = ((App)App.Current).GeneralTouristAttractions;
+
+
     }
+
+   
+   
+
 }
